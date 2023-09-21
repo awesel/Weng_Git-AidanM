4c51f6fd566c6a6498f7a52258c9a0ca501a6b70
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-21 10:06:02 -0700

    Created Commit.java
    added some variables, method headers, and method

---
+++ Commit.java
@@ -0,0 +1,30 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Commit {
+
+    String author;
+    String date;
+    String summary;
+
+    // no previous
+    public Commit() {
+
+    }
+
+    public Commit(String previous) {
+
+    }
+
+    public String sha1(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+}
