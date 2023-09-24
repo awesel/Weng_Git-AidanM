a4f11045999d493f587733732f0d3592cd7f1e29
BenR <90284651+AsianUser@users.noreply.github.com>
2023-09-23 19:12:31 -0700

    Created getDate ()
    gets date & returns as a formatted string

--- Commit.java
+++ Commit.java
@@ -1,5 +1,10 @@
+
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class Commit {
 
@@ -7,24 +12,39 @@ public class Commit {
     String date;
     String summary;
 
+    String treeHash;
+
     // no previous
-    public Commit() {
+    public Commit(String author, String summary) {
+        // create commit file
 
     }
 
-    public Commit(String previous) {
+    // yes previous
+    public Commit(String author, String summary, String previous) {
+        // create commit file
 
     }
 
-    public String sha1(String input) throws NoSuchAlgorithmException {
-        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
-        byte[] result = mDigest.digest(input.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < result.length; i++) {
-            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-        }
+    // creates file and puts in objects folder
+    public void commit(String treePath) throws NoSuchAlgorithmException, IOException {
 
-        return sb.toString();
+        // hash tree
+        treeHash = Blob.sha1(Blob.read(treePath));
+
+        // blobify commit file
+        // read & skips line 3
+
+    }
+
+    public String getDate() {
+        // long milliTime = System.currentTimeMillis();
+
+        LocalDateTime ldt = LocalDateTime.now();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
+
+        return (ldt.format(dtf));
     }
 
 }
